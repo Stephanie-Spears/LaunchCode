2f48278083519173ca06f3c28583e7909a220c3c
Stephanie Spears <stephanie.spears1@gmail.com>
2017-04-22 15:42:38 -0700

    draw squares

--- functionDrawings.py
+++ functionDrawings.py
@@ -1,21 +1,22 @@
 import turtle
 
 def drawSquare(t, sz):
-    """Get turtle t to draw a square with sz side"""
+    """Make turtle t draw a square with side sz."""
     for i in range(4):
         t.forward(sz)
         t.left(90)
 
-wn = turtle.Screen()
+wn = turtle.Screen()       # Set up the window and its attributes
 wn.bgcolor("lightgreen")
-wn.setworldcoordinates(0,0)
 
-alex = turtle.Turtle()
-alex.color("pink")
-for i in range(6):
+alex = turtle.Turtle()     # create alex
+alex.color('hotpink')
+alex.pensize(3)
+
+for i in range(5):
+    drawSquare(alex, 20)   # Call the function to draw the square
     alex.penup()
-    alex.forward(40)
+    alex.forward(40)       # move alex to the starting position for the next square
     alex.pendown()
-    drawSquare(alex,20)
 
 wn.exitonclick()
