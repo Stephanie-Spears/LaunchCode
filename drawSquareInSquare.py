e478c9d5ca1e9be7baf7608957a7878ba3b0d4f0
Stephanie Spears <stephanie.spears1@gmail.com>
2017-04-23 19:16:46 -0700

    draw squares in squares

--- drawSquareInSquare.py
+++ drawSquareInSquare.py
@@ -1,9 +1,19 @@
 import turtle
 
-def drawSquare(myTurt, sideLength):
+def drawSquare(myTurt, sideLen):
     for i in range(4):
-        myTurt.forward(sideLength)
+        myTurt.forward(sideLen)
         myTurt.left(90)
+        
+def fence(myTurt, sideLen):
+    for i in range(4):
+        newXcor = myTurt.xcor()
+        newYcor = myTurt.ycor()
+        newPos = drawSquare(myTurt, sideLen)
+        myTurt.penup()
+        myTurt.goto(newXcor-10, newYcor-10)
+        myTurt.pendown()
+        sideLen+=20
     
 
 wn = turtle.Screen()
@@ -12,8 +22,5 @@ wn.bgcolor("lightgreen")
 fabio = turtle.Turtle()
 fabio.pensize(3)
 fabio.color("hotpink")
-sideLen = 20
-for i in range(5):
-    drawSquare(fabio, sideLen)
-    fabio.forward(sideLen)
-    sideLen += 20
+sideLen=20
+fence(fabio, sideLen)
