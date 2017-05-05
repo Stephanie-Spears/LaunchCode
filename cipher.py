12c749d349f34f9a13fcd8f4d9c814385a2e92ff
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-05-04 22:42:49 -0700

    cipher notes

--- cipher.py
+++ cipher.py
@@ -2,16 +2,34 @@ import string
 from random import shuffle
 
 #alpha = string.ascii_lowercase+string.ascii_uppercase
+plainText = raw_input("What is your plaintext? ")
+shift = int(raw_input("What is your shift? "))
 
+def caesar(plainText, shift): 
 
-def caeserShiftSub(secretWord, shift):
+    for ch in plainText:
+        if ch.isalpha():
+            stayInAlphabet = ord(ch) + shift 
+            if stayInAlphabet > ord('z'):
+                stayInAlphabet -= 26
+            finalLetter = chr(stayInAlphabet)
+        cipherText = ""
+        cipherText += finalLetter
+
+    print ("Your ciphertext is: ", cipherText)
+
+    return cipherText
+
+caesar(plainText, shift)
+
+#def caesarShiftSub(secretWord, shift):
     #CaeserShift with Substition Cipher--with Caeser Shift we need only know the number of rotations to decrypt
     #Adding the Substitution Cipher makes it necessary to know the full list of letter substitutions
     
-    alpha = string.ascii_lowercase+string.ascii_uppercase
-    shift_alpha = alpha[shift:] + alpha[:shift]
-    encodingTable = str.maketrans(alpha, shift_alpha)
-    return secretWord.translate(encodingTable)
+ #   alpha = string.ascii_lowercase+string.ascii_uppercase
+   # shift_alpha = alpha[shift:] + alpha[:shift]
+  #  encodingTable = str.maketrans(alpha, shift_alpha)
+    #return secretWord.translate(encodingTable)
     
 
 
@@ -20,8 +38,8 @@ def caeserShiftSub(secretWord, shift):
 #  print(cipher)
 # print(decipher)
     
-secretWord = "Wolf"
+secretWord = "wolf"
 shift = 2
 #encodingTable = caeserShiftSub(secretWord, shift)
-print(caeserShiftSub(secretWord,shift))
+print(caesarShiftSub(secretWord,shift))
 #rosetta(secretWord, encodingTable)
